d8089b91ea630230a04be6b2dd21c3283757f99f
TLCABHI <77909153+TLCABHI@users.noreply.github.com>
2021-01-25 18:27:04 +0530

    Update ind.js

--- language/ind.js
+++ language/ind.js
@@ -23,7 +23,7 @@ exports.lvlnoon = () => {
 }
 
 exports.noregis = () => {
-	return`*「 NOT REGISTERED 」*\n\n*cara daftar ${prefix}daftar name|Age* \n*example ${prefix}daftar ABHI|18*`
+	return`*「 NOT REGISTERED 」*\n\n*To register ${prefix}daftar name|Age* \n*example ${prefix}daftar ABHI|18*`
 }
 
 exports.rediregis = () => {
